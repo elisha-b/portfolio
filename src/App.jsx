108da74932da5e4f8ab7d411b80e72f3cc4e540d
Elisha Balat <elisha.balat08@gmail.com>
2025-03-20 10:05:16 +1030

    Initial loading of files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() { 
+  return (
+    <>
+      <div>
+        <div>
+          <p class="text-[150px]">&#62; Hi, I'm <span class="text-[#33FFCF]">Elisha Balat.</span></p>
+        </div>
+      
+        <div>
+          <p class="text-[48px]">Aspiring software engineer/developer.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
